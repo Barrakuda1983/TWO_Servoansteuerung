9f18bc0195e13cf5dd76882557ec2e2ff3afe9a0
Barrakuda1983 <darkside1983@web.de>
2023-05-07 12:55:57 +0200

    Anpassung Code-Beschreibung

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
+/*
+Software für Arduino Nano (Uno) zur Ansteuerung von 2x Servomotoren für eine Ventilsteuerung.
+Für TWO.
+*/
+
+
 #include <Arduino.h>
 #include <Servo.h>
 
-Servo servo1;  // Erstellen von Objekten für die beiden Servomotoren
+// Erstellen von Objekten für die beiden Servomotoren
+Servo servo1;  
 Servo servo2;
 
 int pot1 = A0;  
@@ -12,33 +19,39 @@ void setup() {
   pinMode(pot1, INPUT);
   pinMode(pot2, INPUT);
   pinMode(centerDet, INPUT);
-  servo1.attach(6, 900, 2100);  // Anschließen der Servomotoren an digitale Pins + min max Werte
+
+  // Anschluss Servomotoren digitale Pins + Einstellung min- max-Werte Servo
+  servo1.attach(6, 900, 2100);
   servo2.attach(9, 900, 2100);
-  // Serial.begin(9600);
+  // Serial.begin(9600);                                  // zum debuggen
 }
 
 void loop() {
-  int pot1ValA = analogRead(pot1);  // Lesen des Werts vom ersten Kreuzpotentiometer PinA
-  int pot2ValA = analogRead(pot2);  // Lesen des Werts vom zweiten Kreuzpotentiometer PinA
-  // Serial.println((String)"Pot1 = " + pot1ValA);
-  // Serial.println((String)"Pot2 = " + pot2ValA);
+  // Lesen Analogwert Kreuz-Hebel-Schalter
+  int pot1ValA = analogRead(pot1);
+  int pot2ValA = analogRead(pot2);
+  // Serial.println((String)"Pot1 = " + pot1ValA);        // zum debuggen
+  // Serial.println((String)"Pot2 = " + pot2ValA);        // zum debuggen
   
   int servo1Pos;
   int servo2Pos;
     
   if (digitalRead(centerDet) == HIGH){
-    servo1Pos = map(pot1ValA, 300, 1023, 0, 180);  // Übersetzung des Potentiometerwerts in einen Servomotorwinkel
-    servo2Pos = map(pot2ValA, 300, 1023, 0, 180);  // Übersetzung des Potentiometerwerts in einen Servomotorwinkel
+    // Übersetzung Analogwerte >> Servowinkel
+    servo1Pos = map(pot1ValA, 300, 1023, 0, 180);
+    servo2Pos = map(pot2ValA, 300, 1023, 0, 180);
   }
   else {
     servo1Pos = 90;
     servo2Pos = 90;
   }
-  // Serial.println((String)"Servo1Pos = " + servo1Pos);
-  // Serial.println((String)"Servo2Pos = " + servo2Pos);
+  // Serial.println((String)"Servo1Pos = " + servo1Pos);  // zum debuggen
+  // Serial.println((String)"Servo2Pos = " + servo2Pos);  // zum debuggen
 
-  servo1.write(servo1Pos);  // Ansteuern des ersten Servomotors
-  servo2.write(servo2Pos);  // Ansteuern des zweiten Servomotors
-
-  delay(30);  // kurze Pause für eine bessere Kontrolle
+  // Ansteuerung Servomotoren
+  servo1.write(servo1Pos);
+  servo2.write(servo2Pos);
+  
+  // Kurze Pause zur besseren Kontrolle, ggf. nach Motortyp anpassen
+  delay(30);
 }
